4f2b9a615d66290ef77b788403337794a771e4a8
AhmedSabbir11 <picturebackup1123@gmail.com>
2024-08-18 01:19:45 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -184,3 +184,13 @@ const autoScrollProjects = function () {
 setInterval(autoScrollProjects, 100); // Adjust interval timing here
 
 
+
+let progressBar = document.querySelector(".progress-bar");
+let documentHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.onscroll = function(){
+	let progress = (scrollY / documentHeight) * 100;
+	progressBar.style.width = progress + "%";
+}
+
+

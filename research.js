74378252dc45a180a206d4eb7b31953340b046b2
AhmedSabbir11 <picturebackup1123@gmail.com>
2024-08-18 13:14:44 +0600

    Update research.js

--- research.js
+++ research.js
@@ -1,7 +1,26 @@
-let progressBar = document.querySelector(".progress-bar");
-let documentHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+// nav background
+let header = document.querySelector("header");
 
-window.onscroll = function(){
-	let progress = (scrollY / documentHeight) * 100;
-	progressBar.style.width = progress + "%";
-}
+window.addEventListener("scroll", () => {
+    header.classList.toggle("shadow", window.scrollY > 0)
+})
+
+//Filter
+$(document).ready(function () {
+    $(".filter-item").click(function () {
+        const value = $(this).attr("data-filter");
+        if (value == "all"){
+            $(".post-box").show("1000")
+        } else{
+            $(".post-box")
+                .not("." + value)
+                .hide(1000);
+            $(".post-box")
+            .filter("." + value)
+            .show("1000")
+        }
+    });
+    $(".filter-item").click(function () {
+        $(this).addClass("active-filter").siblings().removeClass("active-filter")
+    });
+});
